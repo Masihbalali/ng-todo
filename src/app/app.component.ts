d0fc6c21f855f1c105f3cfa44977086f27e48397
Masihbalali <Masihbalali@outlook.com>
2025-01-16 22:20:15 +0330

    ng g c componentName

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { mainSectionComponent } from './mainSection/mainSection.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, mainSectionComponent],
+  imports: [RouterOutlet, HeaderComponent, mainSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-todo';
+  bindingValueFromParent = signal("binfing value from parrent to footer.")
 }
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  label = signal("signal...")
+  bindingValue = input("default value...")
+}
